67bd99fa363a600dda1a215b95352b5a6cd0dc8b
deadsesh <abysswalk3r69@gmail.com>
2021-03-21 20:44:43 +0200

    Added image imports.

--- src/resources/mock/gallery.js
+++ src/resources/mock/gallery.js
@@ -1,106 +1,107 @@
 import { v4 as uuid } from 'uuid';
+import images from "../img";
 
 export const galleryMock = [
     {
         id: uuid(),
         title: "Cat",
-        src: "./img/cat.png",
+        src: images.cat,
         tag: "art"
     },
     {
         id: uuid(),
         title: "Doberman",
-        src: "./img/doberman.png",
+        src: images.doberman, //"./img/doberman.png",
         tag: "art"
     },
     {
         id: uuid(),
         title: "FLCL",
-        src: "./img/flcl.png",
+        src: images.flcl, //"./img/flcl.png",
         tag: "art"
     },
     {
         id: uuid(),
         title: "FTP",
-        src: "./img/ftp.png",
+        src: images.ftp, //"./img/ftp.png",
         tag: "art"
     },
     {
         id: uuid(),
         title: "Grime",
-        src: "./img/grime.png",
+        src: images.grime, //./img/grime.png",
         tag: "art"
     },
     {
         id: uuid(),
         title: "half",
-        src: "./img/half.png",
+        src: images.half, //"./img/half.png",
         tag: "art"
     },
     {
         id: uuid(),
         title: "Heart",
-        src: "./img/heart.png",
+        src: images.heart, //"./img/heart.png",
         tag: "art"
     },
     {
         id: uuid(),
         title: "Nevermore",
-        src: "./img/nevermore.png",
+        src: images.nevermore, //"./img/nevermore.png",
         tag: "art"
     },
     {
         id: uuid(),
         title: "Raven",
-        src: "./img/Raven.png",
+        src: images.raven, //"./img/Raven.png",
         tag: "art"
     },
     {
         id: uuid(),
         title: "Shiba-Inu",
-        src: "./img/shibe.png",
+        src: images.shibe, //"./img/shibe.png",
         tag: "art"
     },
     {
         id: uuid(),
         title: "VS",
-        src: "./img/star-vs-gif.gif",
+        src: images.vs, //"./img/star-vs-gif.gif",
         tag: "art"
     },
     {
         id: uuid(),
         title: "VG",
-        src: "./img/vg.png",
+        src: images.vg, //"./img/vg.png",
         tag: "art"
     },
     {
         id: uuid(),
         title: "Weeb shit",
-        src: "./img/weeb-shit.jpg",
+        src: images.weeb, //"./img/weeb-shit.jpg",
         tag: "art"
     },
     {
         id: uuid(),
         title: "C#",
-        src: "./img/cs.png",
+        src: images.cs, //"./img/cs.png",
         tag: "programming"
     },
     {
         id: uuid(),
         title: "Python",
-        src: "./img/python.png",
+        src: images.python, //"./img/python.png",
         tag: "programming"
     },
     {
         id: uuid(),
         title: "React",
-        src: "./img/react.png",
+        src: images.react, //"./img/react.png",
         tag: "programming"
     },
     {
         id: uuid(),
         title: "IDK",
-        src: "./img/sample.jpg",
+        src: images.idk, //"./img/sample.jpg",
         tag: "programming"
     }
 ]
