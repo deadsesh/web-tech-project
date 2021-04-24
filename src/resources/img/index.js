5a04a572de7d54937064794f5bdc71a17d5829b1
deadsesh <abysswalk3r69@gmail.com>
2021-04-24 18:28:09 +0300

    Minor refactoring.

---
+++ src/resources/img/index.js
@@ -0,0 +1,38 @@
+import * as cat from './cat.png';
+import * as cs from './cs.png';
+import * as doberman from './doberman.png';
+import * as flcl from './flcl.png';
+import * as ftp from './ftp.png';
+import * as grime from './grime.png';
+import * as half from './half.png';
+import * as heart from './heart.png';
+import * as nevermore from './nevermore.png';
+import * as python from './python.png';
+import * as raven from './raven.png';
+import * as react from './react.png';
+import * as idk from './sample.jpg';
+import * as shibe from './shibe.png';
+import * as vs from './star-vs-gif.gif';
+import * as vg from './vg.png';
+import * as weeb from './weeb-shit.jpg';
+
+const images = {
+    cat,
+    cs,
+    doberman,
+    flcl,
+    ftp,
+    grime,
+    half,
+    heart,
+    nevermore,
+    python,
+    raven,
+    react,
+    idk,
+    shibe,
+    vs,
+    vg,
+    weeb
+}
+export default images;
